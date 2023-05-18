4fa33cf8e5d680acf6e18d616b4501faa46dd249
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-05-18 13:21:54 +0200

    fixes the reload issue and updates the socket to version 0.3.0

--- src/utils/reload.js
+++ src/utils/reload.js
@@ -54,10 +54,10 @@ let scanInterval = 500
 let _opts = {}
 
 let _copyPath = undefined
-let _debounce_handle = null;
+let _debounce_handle = null
 
-const recursePath = async(path, file_fn, data) => {
-  for (const entry of (await fs.readdir(path, {withFileTypes: true}))) {
+const recursePath = async (path, file_fn, data) => {
+  for (const entry of await fs.readdir(path, { withFileTypes: true })) {
     let entry_path = Path.join(path, entry.name)
     if (entry.isDirectory()) {
       await recursePath(entry_path, file_fn, data)
@@ -82,7 +82,7 @@ async function exists(name) {
 async function mkdir(folder) {
   // folder = folder.replaceAll('\\\\', '\\')
   if (await exists(folder)) {
-    console.log(`${folder} exists`)
+    // console.log(`${folder} exists`)
     return
   } else {
     // console.log(`mkdir ${Path.dirname(folder)}`)
@@ -105,7 +105,7 @@ const enableSocketReload = async (opts = {}) => {
 
   if (opts.enable) {
     if (!opts.startDir) {
-      throw "startDir must be defined to monitor for file changes."
+      throw 'startDir must be defined to monitor for file changes.'
     }
   }
 
@@ -113,43 +113,44 @@ const enableSocketReload = async (opts = {}) => {
     opts.liveReload = false
   }
 
-  console.log(`liveReload: ${opts.liveReload}`)
+  // console.log(`liveReload: ${opts.liveReload}`)
 
-  _opts = opts;
+  _opts = opts
 
-  if (enabled === opts.enable)
-    return
+  if (enabled === opts.enable) return
 
   if (opts.enable) {
-    console.log(`platform: ${os.platform()}`)
+    // console.log(`platform: ${os.platform()}`)
     let osParent = '' // define os specific parent path
     os.platform() === 'darwin' && (osParent = '../')
     os.platform() === 'linux' && (osParent = '../')
     osParent = `${osParent}../../../..`
-    let parentPath = Path.join(_opts.startDir, `${osParent}../../../..`);
-    console.log(`ini path: ${`${parentPath}/socket.ini`}`)
+    let parentPath = Path.join(_opts.startDir, `${osParent}../../../..`)
+    // console.log(`ini path: ${`${parentPath}/socket.ini`}`)
     let _parentPath = Path.resolve(Path.join(_opts.startDir, `${osParent}`))
-    if (os.platform() === 'win32' && (_parentPath.startsWith('/')) || (_parentPath.startsWith('\\')))
+    if ((os.platform() === 'win32' && _parentPath.startsWith('/')) || _parentPath.startsWith('\\'))
       _parentPath = _parentPath.substring(1)
     // _copyPath = Path.join(_parentPath, 'src')
     _copyPath = Path.join(_parentPath, window.__args.config.build_copy.replaceAll('"', '').trim())
 
-    console.log(`enableSocketReload: ${opts.enable}, _path: ${_copyPath} => ${_opts.startDir}`)
-    
+    // console.log(`enableSocketReload: ${opts.enable}, _path: ${_copyPath} => ${_opts.startDir}`)
+
     if (opts.debounce === undefined) {
       opts.debounce = -1
     }
 
     if (opts.scanInterval === undefined) {
-      opts.scanInterval = scanInterval;
+      opts.scanInterval = scanInterval
     }
 
     if (!opts.updateCallback) {
-      opts.updateCallback = () => { window.location.reload() }
+      opts.updateCallback = () => {
+        window.location.reload()
+      }
     }
 
-    window.addEventListener("keydown", (event) => {
-      if(((event.ctrlKey || event.metaKey) && event.key === 'r') || event.key === 'F5') {
+    window.addEventListener('keydown', (event) => {
+      if (((event.ctrlKey || event.metaKey) && event.key === 'r') || event.key === 'F5') {
         event.preventDefault()
         reload()
       }
@@ -163,7 +164,7 @@ const enableSocketReload = async (opts = {}) => {
     enabled = true
   } else {
     clearInterval(_interval)
-    enabled = false;
+    enabled = false
   }
 }
 
@@ -174,11 +175,9 @@ const reload = async () => {
 }
 
 async function checkPath(filename) {
-  
   try {
     await fs.stat(filename)
-  } catch (_) {
-  }
+  } catch (_) {}
 }
 
 const sscBuildOutput = async (dest) => {
@@ -191,45 +190,49 @@ const sscBuildOutput = async (dest) => {
   checkPath(dest)
   checkPath(_copyPath)
 
-  await recursePath(_copyPath, async (file, data) => {
-    let dest_path = file.replace(data.base, data.dest)
-    checkPath(dest_path)
-    let update = false
-    let exists = false
-    try {
-      exists = await fs.access(dest_path) // todo(mribbons) fs.access should not throw exception
-    } catch {
-      // console.log(`dest doesn't exist: ${dest_path}`);
-    }
-    checkPath(file)
-    let stat1 = await fs.stat(file);
-    if (exists) {
-      let stat2 = await fs.stat(dest_path);
-
-      if (stat1.mtimeMs > stat2.mtimeMs || stat1.size !== stat2.size) {
-        // console.log(`update ${file}: ${stat1.mtimeMs} > ${stat2.mtimeMs}`)
-        update = true;
+  await recursePath(
+    _copyPath,
+    async (file, data) => {
+      let dest_path = file.replace(data.base, data.dest)
+      checkPath(dest_path)
+      let update = false
+      let exists = false
+      try {
+        exists = await fs.access(dest_path) // todo(mribbons) fs.access should not throw exception
+      } catch {
+        // console.log(`dest doesn't exist: ${dest_path}`);
       }
-      //  else {
-      //   console.log(`ok ${file}: ${stat1.mtimeMs} <= ${stat2.mtimeMs}`)
-      // }
-    } else {
-      // console.log(`not in dest ${file} => ${dest_path}`)
-      update = true
-    }
-    
-    if (update) {
-      // todo(@mribbons): fs.mkdir(dirname(dest_path)) - dirname / drive letter issue on win32
-      // console.log(`copy file: ${file} -> ${dest_path}`)
-      // todo(@mribbons) - copyFile and utimes are noops. Without utimes we can't check times are even, only newer, which isn't great
-      // await fs.copyFile(file, dest_path)
-      // await fs.utimes(dest_path, parseInt(stat1.mtimeMs), parseInt(stat1.mtimeMs))
-      // ensure parent exists
-      await mkdir(Path.dirname(dest_path))
-      await fs.writeFile(dest_path, await fs.readFile(file));
-      data.changed = true
-    }
-  }, recurseData)
+      checkPath(file)
+      let stat1 = await fs.stat(file)
+      if (exists) {
+        let stat2 = await fs.stat(dest_path)
+
+        if (stat1.mtimeMs > stat2.mtimeMs || stat1.size !== stat2.size) {
+          // console.log(`update ${file}: ${stat1.mtimeMs} > ${stat2.mtimeMs}`)
+          update = true
+        }
+        //  else {
+        //   console.log(`ok ${file}: ${stat1.mtimeMs} <= ${stat2.mtimeMs}`)
+        // }
+      } else {
+        // console.log(`not in dest ${file} => ${dest_path}`)
+        update = true
+      }
+
+      if (update) {
+        // todo(@mribbons): fs.mkdir(dirname(dest_path)) - dirname / drive letter issue on win32
+        // console.log(`copy file: ${file} -> ${dest_path}`)
+        // todo(@mribbons) - copyFile and utimes are noops. Without utimes we can't check times are even, only newer, which isn't great
+        // await fs.copyFile(file, dest_path)
+        // await fs.utimes(dest_path, parseInt(stat1.mtimeMs), parseInt(stat1.mtimeMs))
+        // ensure parent exists
+        await mkdir(Path.dirname(dest_path))
+        await fs.writeFile(dest_path, await fs.readFile(file))
+        data.changed = true
+      }
+    },
+    recurseData
+  )
 
   // console.log(`recurse data changed: ${recurseData.changed}`)
   return recurseData.changed
@@ -237,21 +240,22 @@ const sscBuildOutput = async (dest) => {
 
 const checkRefresh = async () => {
   try {
-    // if (await sscBuildOutput(_opts.startDir)) {
-    //   if (_opts.debounce > -1) {
-    //     clearTimeout(_debounce_handle)
-    //     // if debounce wait to update, other updates will reset update timeout
-    //     setTimeout(() => {_opts.updateCallback()}, _opts.debounce)
-        
-    //     // Let consumer know that an update is coming
-    //     if (_opts.debounceCallback) {
-    //       _opts.debounceCallback();
-    //     }
-    //   } else {
-    //     await _opts.updateCallback()
-    //     application.backend.close()
-    //   }
-    // }
+    if (await sscBuildOutput(_opts.startDir)) {
+      if (_opts.debounce > -1) {
+        clearTimeout(_debounce_handle)
+        // if debounce wait to update, other updates will reset update timeout
+        setTimeout(() => {
+          _opts.updateCallback()
+        }, _opts.debounce)
+        // Let consumer know that an update is coming
+        if (_opts.debounceCallback) {
+          _opts.debounceCallback()
+        }
+      } else {
+        await _opts.updateCallback()
+        application.backend.close()
+      }
+    }
   } catch (e) {
     console.log(e)
   }
