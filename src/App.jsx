12ba7a13cdf3203127a107ef94a8cca806a96f9f
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-06-04 15:26:00 +0200

    implement generic version of useAppLoad hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { useSelector } from 'react-redux'
 
 // custom hooks
 import { useAppLoad } from './hooks/useAppLoad'
+import { useActions } from './hooks/useActions'
 
 // Components
 import { SplashScreen, Commander } from './components'
 
 const App = () => {
   const { isLoading } = useSelector((state) => state.mobilePlatforms)
+  const { loadAppError, loadAppSuccess } = useActions()
   // Initial Loading
-  useAppLoad()
+  // As a parameter hook can accept loading time , by default is 3s
+  // onLoadSuccess and onLoadError callbacks
+  useAppLoad(5000, loadAppSuccess, loadAppError)
 
   return <>{isLoading ? <SplashScreen /> : <Commander />}</>
 }
