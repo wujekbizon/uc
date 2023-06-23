beb94395f48f116d9b3234ca3e4ed5d366ef17e2
Mike Ribbons <mribbons@gmail.com>
2023-06-23 20:40:36 +1000

    filesystem: apple: fix issues with non block devices

--- cxx/filesystem.cpp
+++ cxx/filesystem.cpp
@@ -25,15 +25,24 @@ namespace rectavalo::filesystem {
     if (hasError(response)) return response;
     auto path = json["path"].asString();
 
+    std::string lastFile;
+
+    try {
+
+    response["pwd"] = std::filesystem::current_path().string();
+
     for (const auto& entry : std::filesystem::directory_iterator(path)) {
       Json::Value result;
-      result["name"] = entry.path().stem().string().c_str();
-      result["ext"] = entry.path().extension().string().c_str();
+      result["name"] = lastFile = entry.path().stem().string();
+      result["ext"] = entry.path().extension().string();
       result["dir"] = std::filesystem::is_directory(entry.path());
-      result["size"] = std::filesystem::file_size(entry.path());
-      result["mtime"] = unixTime(std::filesystem::last_write_time(entry.path()));
+      result["size"] = entry.is_regular_file() ? (uint64_t)entry.file_size() : 0;
+      result["mtime"] = entry.is_regular_file() ? unixTime(std::filesystem::last_write_time(entry.path())) : 0;
 
       response["result"].append(result);
+    }
+    } catch (std::runtime_error e) {
+      response["error"] = lastFile  + ": " + e.what();
     }
 
     return response;
