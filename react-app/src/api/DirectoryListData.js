15a7c645e43a811cdf2519a9254bc481328b5b67
Mike Ribbons <mribbons@gmail.com>
2023-10-07 11:05:47 +1100

    sort: fix directory entries missing up sort order

--- react-app/src/api/DirectoryListData.js
+++ react-app/src/api/DirectoryListData.js
@@ -17,38 +17,33 @@ export const FILE_SORT_MODE_DATE      = 5
    * @returns 
    */
 function sortDirectories (a, b) {
-  if (a.isDirectory && b.isDirectory) return a.name.localeCompare(b.name)
-  // always sort directories to top
-  if (a.isDirectory || b.isDirectory) return 1
+  if (a.isDirectory && b.isDirectory) {
+    if (a.mtimeMs === 0 || b.mtimeMs === 0)
+      // sort by name if either timestamps have no timestamp
+      return sortName(a, b)
+  }
+  if (a.isDirectory &&  !b.isDirectory) return -1;
+
+  return 1;
 
   return undefined;
 }
 
 function sortName (a, b) {
-  let r = sortDirectories(a, b)
-  if (r !== undefined) return r
-
   return a.name.localeCompare(b.name)
 }
 
 function sortExtension (a, b) {
-  let r = sortDirectories(a, b)
-  if (r !== undefined) return r
-
   return a.ext.localeCompare(b.ext)
 }
 
-function sortSize (a, b) {    
-  let r = sortDirectories(a, b)
-  if (r !== undefined) return r
+function sortSize (a, b) {
   if (a.size > b.size) return 1;
   if (b.size > a.size) return -1;
   return 0
 }
 
-function sortDate (a, b) {    
-  let r = sortDirectories(a, b)
-  if (r !== undefined) return r
+function sortDate (a, b) {
   if (a.mtimeMs > b.mtimeMs) return 1;
   if (b.mtimeMs > a.mtimeMs) return -1;
   return 0
@@ -133,7 +128,6 @@ export default class DirectoryListData {
    * @param {Array} entries 
    */
   sort(entries) {
-
     switch (this.sortMode) {
       case FILE_SORT_MODE_NAME:
         entries.sort(sortName)
@@ -154,6 +148,8 @@ export default class DirectoryListData {
     if (this.sortDirection === SORT_DESCENDING)
       entries = entries.reverse()
 
+    // todo(@mribbons): this should be optional
+    entries.sort(sortDirectories)
     return entries
   }
 
